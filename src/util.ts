07fc62437f27f6dac91da814c92c9a94297de548
Jason Poon <git@jasonpoon.ca>
2016-08-31 12:35:54 -0700

    Use angle bracket notation replacing ctrl+* => <C-*>, backspace => <BS>, etc. Fixes #64

--- src/util.ts
+++ src/util.ts
@@ -9,3 +9,19 @@ export async function showInfo(message : string): Promise<{}> {
 export async function showError(message : string): Promise<{}> {
   return vscode.window.showErrorMessage("Vim: " + message);
 }
+
+export function translateToKeyNotation(key: string): string {
+    const keyNotationTranslations = {
+      'ctrl+' : 'C-',
+      'escape': 'Esc',
+      'backspace': 'BS',
+      'delete': 'Del',
+    };
+
+    let bracketedKey = `<${ key.toLowerCase() }>`;
+    for (let searchKey in keyNotationTranslations) {
+      bracketedKey = bracketedKey.replace(searchKey, keyNotationTranslations[searchKey]);
+    }
+
+    return bracketedKey;
+}
